c45e45b5619191cc0b0fae7428e6ac5ee87035cc
6620 <justin.du@tradevan.com.tw>
2024-06-05 12:18:01 +0800

    feat: init project

---
+++ src/main/java/com/tradevan/switchgamestore/SwitchGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tradevan.switchgamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchGameStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchGameStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tradevan/switchgamestore/SwitchGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradevan.switchgamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchGameStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
