f475eb3ecb10d5dcdf310fdc91c40d8a93e3df2c
6620 <justin.du@tradevan.com.tw>
2024-06-20 10:44:42 +0800

    feat: 調整pom

--- src/test/java/com/tradevan/switchgamestore/ApplicationTests.java
+++ src/test/java/com/tradevan/switchgamestore/ApplicationTests.java
@@ -1,5 +1,9 @@
 package com.tradevan.switchgamestore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,12 @@ class ApplicationTests {
 
     @Test
     void contextLoads() {
+        String actual = "123";
+        List<String> expected = new ArrayList<>();
+
+        assertThat(expected).hasSize(0);
+
+        assertThat(actual).hasToString("123");
     }
 
 }
