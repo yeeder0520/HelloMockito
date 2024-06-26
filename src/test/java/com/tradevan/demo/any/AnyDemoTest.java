4b14af653b57f31d9b761461493466e022ba2606
6620 <justin.du@tradevan.com.tw>
2024-06-26 17:14:11 +0800

    refactor: add any(Class) + eq() 演示

--- src/test/java/com/tradevan/demo/any/AnyDemoTest.java
+++ src/test/java/com/tradevan/demo/any/AnyDemoTest.java
@@ -2,6 +2,7 @@ package com.tradevan.demo.any;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 
 import com.tradevan.demo.Calculator;
@@ -22,13 +23,29 @@ public class AnyDemoTest {
   private Calculator calculator;
 
   @Test
-  public void test_multiply_when_any() {
+  void test_multiply_when_any() {
     // given
     int input1 = 2;
     int input2 = 3;
     String operation = "multiply";
     Integer expected = 6;
     given(calculator.multiply(any(), any())).willReturn(6);
+
+    // when
+    Integer actual = cashier.useCalculate(operation, input1, input2);
+
+    // then
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void test_multiply_when_any_and_eq(){
+    // given
+    int input1 = 9;
+    int input2 = 3;
+    String operation = "multiply";
+    Integer expected = 27;
+    given(calculator.multiply(anyInt(),anyInt() )).willReturn(27);
 
     // when
     Integer actual = cashier.useCalculate(operation, input1, input2);
