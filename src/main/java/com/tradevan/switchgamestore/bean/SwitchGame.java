6b9fd5668153b23b022e02e62d5e98bc3411e22a
6620 <justin.du@tradevan.com.tw>
2024-06-28 10:58:10 +0800

    refactor: changer getter and setter

--- src/main/java/com/tradevan/switchgamestore/bean/SwitchGame.java
+++ src/main/java/com/tradevan/switchgamestore/bean/SwitchGame.java
@@ -1,32 +1,16 @@
 package com.tradevan.switchgamestore.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class SwitchGame {
 
   private String gameName;
   private int price;
   private int stock;
 
-  public String getGameName() {
-    return gameName;
-  }
-
-  public void setGameName(String gameName) {
-    this.gameName = gameName;
-  }
-
-  public int getPrice() {
-    return price;
-  }
-
-  public void setPrice(int price) {
-    this.price = price;
-  }
-
-  public int getStock() {
-    return stock;
-  }
-
-  public void setStock(int stock) {
-    this.stock = stock;
-  }
 }
